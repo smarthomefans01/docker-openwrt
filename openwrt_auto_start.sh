d65cdae845f5d8be555643cce9f96c7efb730a37
smarthomefans01 <98253585+smarthomefans01@users.noreply.github.com>
2023-10-21 17:18:59 +0800

    Update openwrt_auto_start.sh

--- openwrt_auto_start.sh
+++ openwrt_auto_start.sh
@@ -41,3 +41,62 @@ else
     echo "创建macnet网络失败!"
     exit 1
 fi
+
+docker pull shashiikora/openwrt-redstone
+docker run --restart always --name openwrt -d --network macnet --privileged shashiikora/openwrt-redstone /sbin/init
+
+if ! command -v nmap &> /dev/null; then
+    echo "请先安装nmap：sudo apt install nmap"
+    exit 1
+fi
+
+echo "获取宿主机的IP地址..."
+
+# 获取宿主机的IP地址
+HOST_IP=$(hostname -I | awk '{print $1}')
+echo "宿主机的IP地址是: $HOST_IP"
+
+# 获取网络部分和路由器IP
+NETWORK_PART=$(echo $HOST_IP | cut -d'.' -f1-3)
+ROUTER_IP="$NETWORK_PART.1"
+echo "网络部分是: $NETWORK_PART"
+echo "路由器IP是: $ROUTER_IP"
+
+# 使用nmap扫描局域网内的活跃主机
+echo "正在扫描局域网内的活跃主机..."
+ACTIVE_IPS=$(nmap -sn $NETWORK_PART.0/24 -oG - | grep "Up$" | awk '{print $2}')
+
+# 从2到254中随机选择一个IP并检查它是否在活跃IP列表中
+while true; do
+    RANDOM_END=$((RANDOM % 253 + 2))
+    CANDIDATE_IP="$NETWORK_PART.$RANDOM_END"
+    if ! echo "$ACTIVE_IPS" | grep -q "$CANDIDATE_IP"; then
+        # 选中的IP不在活跃列表中
+        OPENWRT_IP=$CANDIDATE_IP
+        break
+    fi
+done
+
+echo "选定的OpenWrt IP地址是: $OPENWRT_IP"
+
+# 使用选中的IP设置OpenWrt的配置
+OPENWRT_CONFIG="
+config interface 'lan'
+        option type 'bridge'
+        option ifname 'eth0'
+        option proto 'static'
+        option ipaddr '$OPENWRT_IP'
+        option netmask '255.255.255.0'
+        option ip6assign '60'
+        option gateway '$ROUTER_IP'
+        option broadcast '$NETWORK_PART.255'
+        option dns '$ROUTER_IP'
+"
+
+# 写入配置到OpenWrt容器
+echo "$OPENWRT_CONFIG" | docker exec -i openwrt bash -c "cat > /etc/config/network"
+
+# 提示用户操作完成
+echo "OpenWrt容器的网络配置已更新。"
+echo "您可以通过以下地址访问OpenWrt: http://$OPENWRT_IP/"
+
